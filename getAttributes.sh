2eb76498ff1a7901588690a1cc2e84c7ddc5e37f
c-0-j-0-c <94489366+c-0-j-0-c@users.noreply.github.com>
2021-11-18 13:09:11 +1000

    Add files via upload

---
+++ getAttributes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+db=$1
+shift
+for var in "$@"
+do
+    sqlite3 $db -cmd ".headers on" -cmd ".mode column" -cmd ".print $var Attributes" ".print ------------------------------------------------------" "pragma table_info($var)" ".print " -cmd ".quit"
+done
+
