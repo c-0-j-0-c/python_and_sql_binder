d0aa7f7261ac670bf1180bf9f5e6f95c20d25ef7
c-0-j-0-c <94489366+c-0-j-0-c@users.noreply.github.com>
2021-11-20 10:08:57 +1000

    Delete getAttributes.sh

--- getAttributes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-db=$1
-shift
-for var in "$@"
-do
-    sqlite3 $db -cmd ".headers on" -cmd ".mode column" -cmd ".print $var Attributes" ".print ------------------------------------------------------" "pragma table_info($var)" ".print " -cmd ".quit"
-
-done
-
