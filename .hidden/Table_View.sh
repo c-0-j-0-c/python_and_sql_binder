8e19c0c4da0f84d0eda41e8eba61878de94e7b33
c-0-j-0-c <94489366+c-0-j-0-c@users.noreply.github.com>
2021-11-20 10:29:21 +1000

    Update Table_View.sh

--- .hidden/Table_View.sh
+++ .hidden/Table_View.sh
@@ -5,6 +5,6 @@ for var in "$@"
 do
 sqlite3 $db -cmd ".headers off" -cmd ".mode list" -cmd ".print $var: " "SELECT name FROM pragma_table_info('$var');" ".print " -cmd ".quit" > ./.hidden/other.txt
 tr -s '\n' ' ' < ./.hidden/other.txt > ./.hidden/output.txt
-python ./.hidden/Tables.py output.txt
+python ./.hidden/Tables.py ./.hidden/output.txt
 echo ""
 done
